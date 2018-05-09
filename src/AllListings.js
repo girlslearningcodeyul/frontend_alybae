ff30c7eb4d76d81cf1c570dd6dab4506f30f9c90
girlslearningcodeyul <ksenia@ladieslearningcode.com>
2018-05-09 13:56:41 -0400

    started on the buy logic

--- src/AllListings.js
+++ src/AllListings.js
@@ -48,7 +48,8 @@ class AllListings extends Component {
                 Description: {contents.description}<br />
                 Price: {contents.price} <br />
                 Sold by: {contents.sellerId} <br />
-            </li>
+                <Link to={"/buy/" + contents.itemId}> Buy this</Link>
+            </li >
 
         let allListings = this.state.searchResults.map(mapContents)
 
