797c6a035d0b697dc22638ba9819f37d9b2d0d6b
girlslearningcodeyul <ksenia@ladieslearningcode.com>
2018-05-09 10:41:01 -0400

    added get items bought

--- src/Account.js
+++ src/Account.js
@@ -6,22 +6,23 @@ class Account extends Component {
     constructor() {
         super();
         this.state = {
-            itemsSold: []
+            itemsSold: [], 
+            itemsBought: []
         }
     }
 
     componentDidMount() {
-        // this.itemsBought();
+        this.itemsBought();
         this.itemsSold();
     }
 
-    // itemsBought = () => {
-    //     fetch('/getItemsBought?userId=' +this.props.username)
-    //         .then(response => response.text())
-    //         .then((items) => {
-    //             this.setState({ itemsBought: JSON.parse(items) })
-    //         })
-    // }
+    itemsBought = () => {
+        fetch('/getItemsBought?userId=' +this.props.username)
+            .then(response => response.text())
+            .then((items) => {
+                this.setState({ itemsBought: JSON.parse(items) })
+            })
+    }
     itemsSold = () => {
         fetch('/getItemsSold?userId=' + this.props.username)
             .then(response => response.text())
@@ -32,34 +33,35 @@ class Account extends Component {
     }
 
     render() {
-        // var mapItemsBought = contents =>
-        //     <li>
-        //         Name: {contents.name} <br />
-        //         Description: {contents.description}<br />
-        //         Price: {contents.price} <br />
-        //         Sold by: {contents.sellerId} <br />
-        //     </li>
+        var mapItemsBought = contents =>
+            <li className="itemsBought">
+                Name: {contents.name} <br />
+                Description: {contents.description}<br />
+                Price: {contents.price} <br />
+                Sold by: {contents.sellerId} <br />
+            </li>
 
         var mapItemsSold = contents =>
-            <li>
+            <li className="itemsSold">
                 Name: {contents.name} <br />
                 Description: {contents.description}<br />
                 Price: {contents.price} <br />
             </li>
 
-        // let itemsBought = this.state.itemsBought.map(mapItemsBought);
+        let itemsBought = this.state.itemsBought.map(mapItemsBought);
         let itemsSold = this.state.itemsSold.map(mapItemsSold);
 
         return (
             <div>
-                {/* Items Bought:
-                <div className="itemsBought">
+                Items Bought:
+                <div>
                     <ul>
                         {itemsBought}
                     </ul>
-                </div> */}
+                </div>
+
                 Items Sold:
-                <div className="itemsSold">
+                <div>
                     <ul>
                         {itemsSold}
                     </ul>
